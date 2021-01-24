b5beaf308b96cb38cb13be07511e8b6b0a6c0fe0
yoggy <yoggy0@gmail.com>
2021-01-24 18:03:37 +0900

    ...

--- mqtt_door_checker.rb
+++ mqtt_door_checker.rb
@@ -9,7 +9,14 @@ require 'ostruct'
 require 'json'
 require 'date'
 
+def usage
+  $stderr.puts "usage : #{File.basename($0)} config.yaml"
+  exit(1)
+end
+
 $prev_hallic = nil
+$last_received_time = Time.now
+$watchdog_thread = nil
 
 $stdout.sync = true
 Dir.chdir(File.dirname($0))
@@ -18,10 +25,13 @@ $current_dir = Dir.pwd
 $log = Logger.new(STDOUT)
 $log.level = Logger::DEBUG
 
-$conf = OpenStruct.new(YAML.load_file("config.yaml"))
+usage if ARGV.size == 0
+
+$conf = OpenStruct.new(YAML.load_file(ARGV[0]))
 
 $conn_opts = {
-  remote_host: $conf.mqtt_host
+  remote_host: $conf.mqtt_host,
+  client_id: $conf.mqtt_client_id
 }
 
 if !$conf.mqtt_port.nil?
@@ -40,19 +50,40 @@ def main_loop
 
     c.subscribe($conf.subscribe_topic)
 
+    $watchdog_thread = Thread.start do
+      loop do
+        sleep($conf.battery_check_interval)
+        if Time.now - $last_received_time > $conf.battery_low_timeout
+          h = {}
+          h["status"] = "unknown"
+          h["last_updated_time"] = DateTime.now.iso8601(0)
+          h["battery"] = "ng"
+          h["target_topic"] = $conf.subscribe_topic
+
+          $log.info "publish: topic=#{$conf.publish_topic}, message=#{h.to_json}"
+          c.publish($conf.publish_topic, h.to_json, true)
+        end
+      end
+    end
+
     c.get do |topic, message|
       $log.info "recv: topic=#{topic}, message=#{message}"
+      $last_received_time = Time.now
+
       json = JSON.parse(message)
       now_hallic = json["HALLIC"]
       now_hallic -= 128 if now_hallic >= 128
 
       if $prev_hallic != nil && now_hallic != $prev_hallic
         h = {}
-        h["is_opened"] = now_hallic == 0 ? true : false
+        h["status"] = now_hallic == 0 ? "open" : "close"
         h["last_updated_time"] = DateTime.now.iso8601(0)
+        h["battery"] = "ok"
+        h["target_topic"] = $conf.subscribe_topic
 
         $log.info "publish: topic=#{$conf.publish_topic}, message=#{h.to_json}"
         c.publish($conf.publish_topic, h.to_json, true)
+
       end
       $prev_hallic = now_hallic
     end
@@ -64,6 +95,12 @@ loop do
     main_loop
   rescue Exception => e
     $log.debug(e.to_s)
+  end
+
+  begin
+    Thread.kill($watchdog_thread) if $watchdog_thread.nil? == false
+    $watchdog_thread = nil
+  rescue
   end
 
   $log.info "waiting 5 seconds..."
